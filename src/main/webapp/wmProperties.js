90e0515239cf42568c3d921607c6c34c36524993
SaraswathiRehala <saraswathi.rekhala+automation@wavemaker.com>
2017-11-22 13:28:47 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_MnuTeGtOFg",
+  "homePage" : "Main",
+  "name" : "Auto_MnuTeGtOFg",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
